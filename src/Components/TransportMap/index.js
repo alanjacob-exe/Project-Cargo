1ba14f5cfd70b001431b5b466f484ec0345ec22c
alanjacob-exe <77325299+alanjacob-exe@users.noreply.github.com>
2022-11-17 13:09:51 +0530

    Polyline Update

--- src/Components/TransportMap/index.js
+++ src/Components/TransportMap/index.js
@@ -4,18 +4,19 @@ import { MapContainer, TileLayer, Marker,Polyline, Popup } from "react-leaflet";
 
 export function TransportMap(position) {
   const polyline = position.pointers
-  const limeOptions = { color: "blue" };
+  const blueOptions = { color: "blue" };
 //   console.log("mapsource in tmaps"+position.mapsource)
 
   return (
-    <MapContainer center={[10.9984717, 76.1401113]} zoom={11}>
+    <MapContainer center={[10.9984717, 76.1401113]} zoom={11}  zoomOffset= {-1}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.thunderforest.com/">Transport Map</a> contributors'
         url="https://tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=b27fc203562944ceb7363792b9e8c9d2"
       />
       <Marker position={position.location1} />
       <Marker position={position.location2} />
-      <Polyline pathOptions={limeOptions} positions={polyline} />
+      <Polyline pathOptions={blueOptions} positions={polyline} />
 
     </MapContainer>
   );
